5b6d3de0b01eec9aba09db14ceed893cc4bb2bd0
Oreva <khemical51@gmail.com>
2024-01-16 18:03:42 +0100

    add navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between p-2 sm:p-4 items-center max-w-6xl mx-auto">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl">
+            <span className="text-slate-500">Gi</span>
+            <span className="text-slate-700">Ti</span>
+          </h1>
+        </Link>
+        <form className="flex items-center bg-slate-100 p-3 rounded-lg">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent outline-none w-24 sm:w-60"
+          />
+          <FaSearch className="text-slate-700" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/about">
+            <li className="text-slate-600 hidden sm:inline hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="text-slate-600 hover:underline">Sign In</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
