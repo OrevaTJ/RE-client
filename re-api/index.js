b64e6d3d1ccc59da4aa2883c435a4b91ebef617e
Oreva <khemical51@gmail.com>
2024-01-19 12:09:08 +0100

    add error middleware and function

--- re-api/index.js
+++ re-api/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -16,7 +16,7 @@ mongoose
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.listen(3001, () => {
   console.log('Server listening on port 3001!!!');
@@ -24,3 +24,14 @@ app.listen(3001, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
+app.use((error, req, res, next) => {
+  const statusCode = error.statusCode || 500;
+  const message = error.message || 'Internal server error';
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
