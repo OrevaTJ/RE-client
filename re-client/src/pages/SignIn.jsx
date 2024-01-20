26333b05b7869f09de13925a1bc7a1c17b430705
Oreva <khemical51@gmail.com>
2024-01-20 11:35:29 +0100

    add redux toolkit and persist

--- re-client/src/pages/SignIn.jsx
+++ re-client/src/pages/SignIn.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux'
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice'
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const {error, loading} = useSelector((state) => state.user)
   const navigate = useNavigate();
+  const dispatch = useDispatch()
 
   const handleChange = (e) => {
     setFormData({
@@ -18,7 +20,7 @@ export default function SignIn() {
     e.preventDefault();
 
     try {
-      setLoading(true);
+      dispatch(signInStart)
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -31,17 +33,14 @@ export default function SignIn() {
 
       // if error from api
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message))
         return;
       }
 
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data))
       navigate('/');
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message))
     }
   };
 
---
+++ re-client/src/redux/store.js
@@ -0,0 +1,43 @@
+// import { configureStore } from '@reduxjs/toolkit'
+// import userReducer from './user/userSlice'
+
+// export const store = configureStore({
+//   reducer: { user: userReducer },
+//   middleware: (getDefaultMiddleware) => {
+//     return getDefaultMiddleware({
+//       serializableCheck: false
+//     })
+//   },
+// })
+
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import userReducer from './user/userSlice';
+
+// Combine reducers into a root reducer
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+// Create a persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure the store with the persisted reducer
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: false,
+    });
+  },
+});
+
+// Create a persistor object
+export const persistor = persistStore(store);
