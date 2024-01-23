33fd42a9e7a7f78d72828d76700d53ecf4bcf2ab
Oreva <khemical51@gmail.com>
2024-01-23 16:08:23 +0100

    update user info

--- re-api/controllers/user.controller.js
+++ re-api/controllers/user.controller.js
@@ -1,3 +1,47 @@
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+import bcryptjs from 'bcryptjs';
+
 export const test = (req, res) => {
-    res.send('Hello World!!!')
-}
+  res.send('Hello World!!!');
+};
+
+export const updateUser = async (req, res, next) => {
+  const { username, email, profilePhoto, password } = req.body;
+  const { id: userId } = req.user;
+  const { id: requestedUserId } = req.params;
+
+  try {
+    if (userId !== requestedUserId)
+      return next(errorHandler(401, 'Update not allowed'));
+
+    let hashedPassword;
+    if (password) {
+      hashedPassword = bcryptjs.hashSync(password, 10);
+    }
+
+    const updateObject = {
+      $set: {
+        username,
+        email,
+        password: hashedPassword,
+        profilePhoto,
+      },
+    };
+
+    const updatedUser = await User.findByIdAndUpdate(
+      requestedUserId,
+      updateObject,
+      {
+        new: true, // return updated document
+        runValidators: true, // enforce validation rules specified in the model's schema
+      }
+    );
+
+    const { password: updatedPassword, ...otherUserDetails } = updatedUser._doc;
+
+    res.status(200).json(otherUserDetails);
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ re-api/utils/verifyToken.js
@@ -0,0 +1,17 @@
+import { errorHandler } from './error.js';
+import jwt from 'jsonwebtoken';
+
+export const verifyToken = (req, res, next) => {
+  try {
+    const token = req.cookies.user_token;
+
+    if (!token) return next(errorHandler(401, 'Unauthorized'));
+
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = user;
+    next();
+  } catch (error) {
+    next(errorHandler(403, error.message));
+  }
+};
