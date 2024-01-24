6860ffad533eb5ce6c9cf3b251f852ed92ba42f1
Oreva <khemical51@gmail.com>
2024-01-24 18:03:52 +0100

    create user listing api

---
+++ re-api/models/listing.model.js
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose';
+
+const listingSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    priceRegular: {
+      type: Number,
+      required: true,
+    },
+    priceDiscounted: {
+      type: Number,
+      required: true,
+    },
+    bedrooms: {
+      type: Number,
+      required: true,
+    },
+    bathrooms: {
+      type: Number,
+      required: true,
+    },
+    furnished: {
+      type: Boolean,
+      required: true,
+    },
+    parking: {
+      type: Boolean,
+      required: true,
+    },
+    offer: {
+      type: Boolean,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    userDetails: {
+      type: String,
+      required: true,
+    },
+    imageUrls: {
+      type: Array,
+      required: true,
+    }
+  },
+  { timestamps: true }
+);
+
+const Listing = mongoose.model('Listing', listingSchema);
+
+export default Listing;
