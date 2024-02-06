40029125ed98e92815d360a2cad62a98a4ee966a
Oreva <khemical51@gmail.com>
2024-02-06 19:02:29 +0100

    add builds

--- re-api/index.js
+++ re-api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 mongoose
@@ -15,6 +16,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+  const __dirname = path.resolve()
 
 const app = express();
 
@@ -29,6 +32,12 @@ app.listen(3001, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/re-client/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 're-client', 'dist', 'index.html'))
+})
 
 app.use((error, req, res, next) => {
   const statusCode = error.statusCode || 500;
