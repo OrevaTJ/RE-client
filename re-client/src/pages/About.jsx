8b1f836f4f627ff1586b95d9e0edf48d8cb9cab2
Oreva <khemical51@gmail.com>
2024-02-06 18:47:59 +0100

    add about page ui

--- re-client/src/pages/About.jsx
+++ re-client/src/pages/About.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-16 px-8 max-w-6xl mx-auto">
+      <h1 className="text-3xl text-center font-bold mb-4">About Konoha Estate</h1>
+      <p className="text-slate-700 mb-4">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
+        unde consequatur? Deleniti cumque nostrum modi assumenda minima
+        voluptatum consequatur esse error nulla beatae molestiae officiis
+        aperiam numquam ex possimus rerum magnam deserunt pariatur, dolores
+        maxime rem! Enim voluptates assumenda libero blanditiis. Sint cum
+        repellat excepturi tempore suscipit sequi nobis nesciunt!
+      </p>
+      <p className="text-slate-700 mb-4">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
+        unde consequatur? Deleniti cumque nostrum modi assumenda minima
+        voluptatum consequatur esse error nulla beatae molestiae officiis
+        aperiam numquam ex possimus rerum magnam deserunt pariatur, dolores
+        maxime rem! Enim voluptates assumenda libero blanditiis. Sint cum
+      </p>
+      <p className="text-slate-700 mb-4">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
+        unde consequatur? Deleniti cumque nostrum modi assumenda minima
+        voluptatum consequatur esse error nulla beatae molestiae officiis
+        aperiam numquam ex possimus rerum magnam deserunt pariatur, dolores
+        maxime rem! Enim voluptates assumenda libero blanditiis. Sint cum
+        repellat excepturi tempore suscipit sequi nobis nesciunt!
+      </p>
+    </div>
+  );
 }
