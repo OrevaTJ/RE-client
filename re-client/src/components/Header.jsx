5d7cae3220786e215d33da62451d22488406e988
Oreva <khemical51@gmail.com>
2024-02-16 19:18:14 +0100

    remove search from header

--- re-client/src/components/Header.jsx
+++ re-client/src/components/Header.jsx
@@ -1,58 +1,48 @@
-import { useEffect, useState } from 'react';
-import { FaSearch } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
+import profilePicPlaceholder from '../../assets/profile-pic-placeholder.png';
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
-  const [searchTerm, setSearchTerm] = useState('');
-  const navigate = useNavigate()
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-
-    const urlParams = new URLSearchParams(window.location.search)
-    urlParams.set('q', searchTerm)
-    const searchQuery = urlParams.toString()
-    navigate(`/search?${searchQuery}`)
-  }
-
-  // Sync the search input to the browser url
-  useEffect(() => {
-    const urlParams = new URLSearchParams(location.search)
-    const urlSearchTerm = urlParams.get('q')
-    setSearchTerm(urlSearchTerm || '')
-  }, [location.search])
-  
 
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between p-2 sm:p-4 items-center max-w-6xl mx-auto">
-        <Link to="/">
+        <Link to="/" className="flex items-center">
+          <img
+            src={logo}
+            alt="Image"
+            className="w-7 h-7 object-contain rounded-md"
+          />
           <h1 className="font-bold text-sm sm:text-xl">
             <span className="text-slate-500">Gi</span>
             <span className="text-slate-700">Ti</span>
           </h1>
         </Link>
-        <form onSubmit={handleSubmit} className="flex items-center bg-slate-100 p-3 rounded-lg">
-          <input
-            type="text"
-            placeholder="Search..."
-            required
-            className="bg-transparent outline-none w-24 sm:w-60"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <button>
-            <FaSearch className="text-slate-700" />
-          </button>
-        </form>
-        <ul className="flex gap-4">
-          <Link to="/about">
-            <li className="text-slate-600 hidden sm:inline hover:underline">
-              About
-            </li>
-          </Link>
+        <div className="flex justify-between items-center gap-8">
+          <ul className="flex gap-4 font-semibold">
+            <Link to="/">
+              <li className="text-slate-600 hidden sm:inline hover:underline">
+                Home
+              </li>
+            </Link>
+            <Link to="/about">
+              <li className="text-slate-600 hover:underline">
+                About
+              </li>
+            </Link>
+            <Link to="#">
+              <li className="text-slate-600 hover:underline">
+                Blog
+              </li>
+            </Link>
+            <Link to="#">
+              <li className="text-slate-600 hover:underline">
+                Contact Us
+              </li>
+            </Link>
+          </ul>
           <Link to="/profile">
             {currentUser ? (
               <img
@@ -61,10 +51,24 @@ export default function Header() {
                 className="object-cover rounded-full w-7 h-7"
               />
             ) : (
-              <li className="text-slate-600 hover:underline">Sign In</li>
+              <>
+                <div className="gap-2 text-white hidden sm:flex ">
+                  <button className="bg-orange-600 hover:bg-orange-700 py-1 px-4 rounded">
+                    Login
+                  </button>
+                  <button className="bg-orange-600 hover:bg-orange-700 py-1 px-4 rounded">
+                    Register
+                  </button>
+                </div>
+                <img
+                  src={profilePicPlaceholder}
+                  alt="Profile Photo"
+                  className="object-cover rounded-full w-7 h-7 sm:hidden"
+                />
+              </>
             )}
           </Link>
-        </ul>
+        </div>
       </div>
     </header>
   );
