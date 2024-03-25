7db561aaba72296f62a43f9cc9067b5a93e59e17
Oreva <khemical51@gmail.com>
2024-03-25 15:44:08 +0100

    add node cron job

--- re-api/index.js
+++ re-api/index.js
@@ -6,7 +6,12 @@ import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import cron from 'node-cron';
+import axios from 'axios'; // Import axios for making HTTP requests
+
 dotenv.config();
+
+const PORT = process.env.PORT || 3001;
 
 mongoose
   .connect(process.env.MONGO)
@@ -25,13 +30,18 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(3001, () => {
-  console.log('Server listening on port 3001!!!');
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}!!!`);
 });
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+// Health check endpoint to verify server status
+app.get('/health', (req, res) => {
+  res.send('Server is running');
+});
 
 app.use(express.static(path.join(__dirname, '/re-client/dist')))
 
@@ -48,4 +58,17 @@ app.use((error, req, res, next) => {
     statusCode,
     message,
   });
+});
+
+// Define a cron job to run every 15 minutes
+cron.schedule('*/15 * * * *', async () => {
+  try {
+    // Make an HTTP request to the health check endpoint
+    const response = await axios.get('https://giti-estate.onrender.com//health');
+    // Log the response data
+    console.log('Health check response:', response.data);
+  } catch (error) {
+    // Log any errors that occur during the health check
+    console.error('Error during health check:', error.message);
+  }
 });
